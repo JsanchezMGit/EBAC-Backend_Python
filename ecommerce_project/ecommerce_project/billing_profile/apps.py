b310d7b31529e78ab972738b18850631ca6c3347
Jesus Sanchez <jesuh_aguilas@msn.com>
2025-04-11 22:42:14 -0600

    Delete ecommerce_project/ecommerce_project directory

--- ecommerce_project/ecommerce_project/billing_profile/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BillingProfileConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'billing_profile'
--- ecommerce_project/ecommerce_project/cart/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from product.models import Product
-
-class Cart(models.Model):
-
- user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-
- products = models.ManyToManyField(Product)
